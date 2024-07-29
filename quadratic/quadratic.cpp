b98a3fbbbbcc6b049358d5ad76d1f44a14589acd
Lebogang Komape <marvenkom272@gmail.com>
2024-07-29 02:06:19 +0200

    Exercise 3.3

--- quadratic/quadratic.cpp
+++ quadratic/quadratic.cpp
@@ -4,8 +4,8 @@
 using namespace std;
 
 int main() {
-    using Complex = complex<float>;
-    int a = 0, b = 0, c = 0;
+    using ComplexNum = complex<float>;
+    int a, b, c;
     char choice;
 
     do {
@@ -16,10 +16,10 @@ int main() {
         cout << "Enter integers coefficient of c: ";
         cin >> c;
 
-        Complex discriminant = sqrt(Complex(pow(b,b) - 4 * a * c, 0));
+        ComplexNum discriminant = sqrt(ComplexNum(pow(b,b) - 4 * a * c, 0));
 
-        Complex plusRoot = ((float)(-1*b) + discriminant) / ((float)2 * a);
-        Complex minusRoot = ((float)(-1*b) - discriminant) / ((float)2 * a);
+        ComplexNum plusRoot = ((float)(-1*b) + discriminant) / ((float)2 * a);
+        ComplexNum minusRoot = ((float)(-1*b) - discriminant) / ((float)2 * a);
 
         cout << "X = " << plusRoot << " or X = " << minusRoot << endl;
         cout << "Do you want to calculate another quadratic? (Enter 'q' to quit): ";
